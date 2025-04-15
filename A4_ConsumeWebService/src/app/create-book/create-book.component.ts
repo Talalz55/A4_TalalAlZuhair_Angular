bd0293416d5b855200aed5ba22e423fb11ceddb5
Talal Al Zuhair <alzuhait@sheridancollege.ca>
2025-04-15 16:44:43 -0400

    feat:changes on create book component

--- A4_ConsumeWebService/src/app/create-book/create-book.component.ts
+++ A4_ConsumeWebService/src/app/create-book/create-book.component.ts
@@ -1,12 +1,22 @@
+// Name: Your Name, Student ID: Your ID
 import { Component } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../book';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-create-book',
-  standalone: true,
-  imports: [],
-  templateUrl: './create-book.component.html',
-  styleUrl: './create-book.component.css'
+    selector: 'app-create-book',
+    templateUrl: './create-book.component.html',
+    styleUrls: ['./create-book.component.css']
 })
 export class CreateBookComponent {
+    book: Book = { id: 0, title: '', authorName: '', price: 0, quantity: 0 };
 
+    constructor(private bookService: BookService, private router: Router) { }
+
+    createBook(): void {
+        this.bookService.createBook(this.book).subscribe(() => {
+            this.router.navigate(['/find']);
+        });
+    }
 }
