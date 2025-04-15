2591ded9225f7149e54f74c2c4d50d5d588b1df6
Talal Al Zuhair <alzuhait@sheridancollege.ca>
2025-04-15 16:48:07 -0400

    feat:change on update book

--- A4_ConsumeWebService/src/app/update-book/update-book.component.ts
+++ A4_ConsumeWebService/src/app/update-book/update-book.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+// Name: Talal Al Zuhair, Student ID: 991658377
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../book';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-update-book',
-  standalone: true,
-  imports: [],
-  templateUrl: './update-book.component.html',
-  styleUrl: './update-book.component.css'
+    selector: 'app-update-book',
+    templateUrl: './update-book.component.html',
+    styleUrls: ['./update-book.component.css']
 })
-export class UpdateBookComponent {
+export class UpdateBookComponent implements OnInit {
+    book: Book = { id: 0, title: '', authorName: '', price: 0, quantity: 0 };
+    edit: boolean = true; // Frontend-specific flag
 
+    constructor(
+        private bookService: BookService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        const id = +this.route.snapshot.paramMap.get('id')!;
+        this.bookService.getBookById(id).subscribe(data => {
+            this.book = data;
+        });
+    }
+
+    updateBook(): void {
+        if (this.edit) {
+            this.bookService.updateBook(this.book.id, this.book).subscribe(() => {
+                this.router.navigate(['/find']);
+            });
+        } else {
+            alert('Update is disabled.');
+        }
+    }
 }
