1cea8c76f6c0315235e5abbe3ed14dd46da41fda
Talal Al Zuhair <alzuhait@sheridancollege.ca>
2025-04-15 16:49:27 -0400

    feat:changes on delete book component

--- A4_ConsumeWebService/src/app/delete-book/delete-book.component.ts
+++ A4_ConsumeWebService/src/app/delete-book/delete-book.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+// Name: Your Name, Student ID: Your ID
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../book';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-delete-book',
-  standalone: true,
-  imports: [],
-  templateUrl: './delete-book.component.html',
-  styleUrl: './delete-book.component.css'
+    selector: 'app-delete-book',
+    templateUrl: './delete-book.component.html',
+    styleUrls: ['./delete-book.component.css']
 })
-export class DeleteBookComponent {
+export class DeleteBookComponent implements OnInit {
+    book: Book | null = null;
 
+    constructor(
+        private bookService: BookService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        const id = +this.route.snapshot.paramMap.get('id')!;
+        this.bookService.getBookById(id).subscribe(data => {
+            this.book = data;
+        });
+    }
+
+    deleteBook(): void {
+        if (this.book && confirm('Are you sure you want to delete this book?')) {
+            this.bookService.deleteBook(this.book.id).subscribe(() => {
+                this.router.navigate(['/find']);
+            });
+        }
+    }
 }
